9f33ce55b43eadc3cb7e6f78ee10f0538443a0a5
Ojas Mittal <ojasfarm31@gmail.com>
2022-03-12 23:18:38 +0530

    initial commit

---
+++ email.py
@@ -0,0 +1,24 @@
+import yagmail
+import pandas
+from newsfeed import NewsFeed
+import datetime
+import time
+
+def send_email():
+
+  today = datetime.datetime.now().strftime('%Y-%m-%d')
+  yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+  news_feed = NewsFeed(interest=row['interest'],
+                       from_date=yesterday,
+                       to_date=today)
+  email = yagmail.SMTP(user="automail.ojas.python@gmail.com", password="ojaspython123")
+  email.send(to=row['email'],
+             subject=f"Your {row['interest']} news for today!",
+             contents=f"Hi {row['name']}\n See what's on about {row['interest']} today. \n{news_feed.get()}")
+
+df = pandas.read_excel()
+for index, row in df.iterrows():
+      send_email()
+
+
+
---
+++ main.py
@@ -0,0 +1,62 @@
+import pyrebase
+import streamlit as st
+import pandas
+import email
+
+
+from datetime import datetime
+
+firebaseConfig = {
+  'apiKey': "AIzaSyBcVEHxxIE2NofVoeoezfdojbree_ur7zY",
+  'authDomain': "firestore-streamlit-97b82.firebaseapp.com",
+  'projectId': "firestore-streamlit-97b82",
+  'databaseURL': "https://firestore-streamlit-97b82-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  'storageBucket': "firestore-streamlit-97b82.appspot.com",
+  'messagingSenderId': "497317595554",
+  'appId': "1:497317595554:web:9f6c1d3840b74879bbcafb",
+  'measurementId': "G-5M4C07T8QN"
+};
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+db=firebase.database()
+storage=firebase.storage()
+st.sidebar.title("Our Project app")
+choice=st.sidebar.selectbox('Login/SignUp',['Login','Sign up'])
+email=st.sidebar.text_input("Enter your email address")
+password=st.sidebar.text_input("Enter your password")
+
+
+
+
+if choice=="Sign up":
+  handle=st.sidebar.text_input("Please enter you name",value="Default")
+  submit=st.sidebar.button('Create my Account')
+  if submit:
+    user=auth.create_user_with_email_and_password(email,password)
+    st.success("Your account is created successfully!")
+    st.balloons()
+    user=auth.sign_in_with_email_and_password(email,password)
+    db.child(user['localId']).child("Handle").set(handle)
+    db.child(user['localId']).child("Id").set(user['localId'])
+    st.title("Welcome"+handle)
+    st.info("Login through login option in drop down menu")
+if choice == "Login":
+      login = st.sidebar.checkbox('Login')
+      if login:
+        user = auth.sign_in_with_email_and_password(email, password)
+        bio = st.radio('Jump to', ['Home', 'Sms Service', 'Email Service'])
+        if bio=="Home":
+          st.info("This app is designed to provide free and bulk sms and email services. Just import your file and enjoy!")
+        if bio == "Email Service":
+          st.title("Drop your excel file below containing user name,interest and email id")
+          uploaded_file = st.file_uploader("Choose a file")
+          if uploaded_file is not None:
+            df = pandas.read_excel(uploaded_file)
+            for index, row in df.iterrows():
+              email.send_email()
+        if bio == "Sms Service":
+          st.title("Drop your excel file below containing user name and mobile no.")
+          uploaded_file = st.file_uploader("Choose a file")
+          if uploaded_file is not None:
+            st.balloons()
