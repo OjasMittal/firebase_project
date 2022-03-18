0da7c0d8a1d0ee6dbd825225b346fc13d771d0fb
Ojas Mittal <ojasfarm31@gmail.com>
2022-03-18 23:58:00 +0530

    added case for invalid secret code

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ st.markdown(hide_menu_style,unsafe_allow_html=True)
 
 
 if choice=="Sign up":
-  handle=st.sidebar.text_input("Please enter you name",value="Ojas")
+  handle=st.sidebar.text_input("Please enter your nickname",value="Ojas")
   submit=st.sidebar.button('Create my Account')
   if submit:
     try:
@@ -63,85 +63,92 @@ if choice=="Sign up":
       db.child(user['localId']).child("Id").set(user['localId'])
       st.title("Welcome " + handle + " !")
     except:
-      st.info("This account already exists")
+      st.info("This account already exists !")
 
     st.info("Login through login option in the left drop down menu to use the services")
 if choice == "Login":
       login = st.sidebar.checkbox('Login')
       if login:
-        user = auth.sign_in_with_email_and_password(email, password)
-        bio = st.radio('Jump to', ['Home', 'Sms Service', 'Email Service'])
-        st.write('<style>div.row-widget.stRadio > div {flex-direction:row;}</style>',unsafe_allow_html=True)
-        if bio=="Home":
-          st.info("This app is designed to provide free and bulk sms and email services. Just import your file and enjoy!")
-        if bio == "Email Service":
-          image2 = Image.open('bulk email.jpg')
-          st.image(image2, caption='Bulk email in a click')
-          workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-          worksheet = workbook.add_worksheet()
+        try:
+          user = auth.sign_in_with_email_and_password(email, password)
+          bio = st.radio('Jump to', ['Home', 'Sms Service', 'Email Service'])
+          st.write('<style>div.row-widget.stRadio > div {flex-direction:row;}</style>',unsafe_allow_html=True)
+          if bio == "Home":
+            st.info(
+              "This app is designed to provide free and bulk sms and email services. Just import your file and enjoy!")
+          if bio == "Email Service":
+            image2 = Image.open('bulk email.jpg')
+            st.image(image2, caption='Bulk email in a click')
+            workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+            worksheet = workbook.add_worksheet()
 
-          worksheet.write('A1', 'Name')
-          worksheet.write('B1', 'Surname')
-          worksheet.write('C1','number')
-          worksheet.write('D1', 'Interest')
-          worksheet.write('E1', 'Email')
-          workbook.close()
+            worksheet.write('A1', 'Name')
+            worksheet.write('B1', 'Surname')
+            worksheet.write('C1', 'number')
+            worksheet.write('D1', 'Interest')
+            worksheet.write('E1', 'Email')
+            workbook.close()
 
-          st.download_button(
-            label="Download Excel template file",
-            data=output.getvalue(),
-            file_name="template.xlsx",
-            mime="application/vnd.ms-excel"
-          )
+            st.download_button(
+              label="Click to download Excel template file to fill and upload",
+              data=output.getvalue(),
+              file_name="template.xlsx",
+              mime="application/vnd.ms-excel"
+            )
 
-          st.title("Download,fill and drop the excel file below ")
-          secret="once"
+            st.title("Download,fill and drop the excel file below ")
+            secret = "once"
 
-          uploaded_file = st.file_uploader("Choose a file",key=secret)
-          if uploaded_file is not None:
-            ef = pandas.read_excel(uploaded_file)
-            for index, row in ef.iterrows():
-              bulk_email.send_email(row)
-              st.write("Emails sent successfully")
-              secret="done"
-              state=st.session_state['key']=secret
+            uploaded_file = st.file_uploader("Choose a file", key=secret)
+            if uploaded_file is not None:
+              ef = pandas.read_excel(uploaded_file)
+              for index, row in ef.iterrows():
+                bulk_email.send_email(row)
+                st.write("Emails sent successfully")
+                secret = "done"
+                state = st.session_state['key'] = secret
+
+          if bio == "Sms Service":
+            image3 = Image.open('bulk sms.jpg')
+            st.image(image3, caption='Bulk SMS in a click')
+            workbook1 = xlsxwriter.Workbook(output, {'in_memory': True})
+            worksheet1 = workbook1.add_worksheet()
+
+            worksheet1.write('A1', 'number')
+            workbook1.close()
 
 
+            title = st.text_input('Enter the special code for using this service in the box  below',)
+            if title == "PLEASE":
 
-        if bio == "Sms Service":
-          image3 = Image.open('bulk sms.jpg')
-          st.image(image3, caption='Bulk SMS in a click')
-          workbook1 = xlsxwriter.Workbook(output, {'in_memory': True})
-          worksheet1 = workbook1.add_worksheet()
+              msg = st.text_input('Enter your message for the receiver in the box  below')
+              st.download_button(
+                label="Click to download Excel template file to fill and upload",
+                data=output.getvalue(),
+                file_name="template.xlsx",
+                mime="application/vnd.ms-excel"
+              )
+              st.title("Drop your excel file below containing  mobile nos. in vertical column")
+              secret = "fun"
+              uploaded_file1 = st.file_uploader("Choose a file", key=secret)
+              if uploaded_file1 is not None:
+                st.balloons()
+                num = ""
+                gf = pandas.read_excel(uploaded_file1)
+                for index, row in gf.iterrows():
+                  a = str(row['number']
+                          )
+                  num += a + ','
+                l = len(num)
+                num = num[:l - 1]
+                sms.bulk_sms(num, msg)
+                st.write("sms sent successfully")
+                secret = "fun_over"
+                state = st.session_state['key'] = secret
+            elif  title!="":
+              st.info("Incorrect Special Code. Contact the developer to know this Special Code!")
 
-          worksheet1.write('A1', 'number')
-          workbook1.close()
-
-          st.download_button(
-            label="Download Excel template file",
-            data=output.getvalue(),
-            file_name="template.xlsx",
-            mime="application/vnd.ms-excel"
-          )
-          title = st.text_input('Enter the special code for using this service in the box  below')
-          if title=="PLEASE":
-            msg=st.text_input('Enter your message in the box  below')
-            st.title("Drop your excel file below containing  mobile nos. in vertical column")
-            secret="fun"
-            uploaded_file1 = st.file_uploader("Choose a file",key=secret)
-            if uploaded_file1 is not None:
-              st.balloons()
-              num = ""
-              gf = pandas.read_excel(uploaded_file1)
-              for index, row in gf.iterrows():
-                a = str(row['number']
-                        )
-                num += a + ','
-              l = len(num)
-              num = num[:l - 1]
-              sms.bulk_sms(num,msg)
-              st.write("sms sent successfully")
-              secret = "fun_over"
-              state = st.session_state['key'] = secret
+        except:
+          st.info("Enter a valid email/password !")
 
 
